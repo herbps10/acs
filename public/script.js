2ace8c0cf2e1079d76b4c03af818bd73c9568916
Herb Susmann <herbps10@gmail.com>
2011-06-23 16:55:51 -0400

    Work on user interface and Facebook API connection. Can now retrieve user information from the FB api

--- public/script.js
+++ public/script.js
@@ -1,8 +1,4 @@
-window.fbAsyncInit = function() {
 
-FB.init({appId: '109163289177099', status: true, cookie: true,
-	 xfbml: true});
-};
 (function() {
 var e = document.createElement('script');
 e.type = 'text/javascript';
@@ -11,3 +7,31 @@ e.src = document.location.protocol +
 e.async = true;
 document.getElementById('fb-root').appendChild(e);
 }());
+
+window.fbAsyncInit = function() {
+	FB.init({appId: '109163289177099', status: true, cookie: true,
+	 	xfbml: true});
+
+	checkLoggedIn();
+
+	updateFromFacebook();
+};
+
+function checkLoggedIn() {
+	FB.getLoginStatus(function(response) {
+		if(response.session) {
+			$(".login").remove();
+		}
+		else {
+			$(".login").show();
+		}
+	});
+}
+
+function updateFromFacebook() {
+	FB.api('/me', function(user) {
+		if(user != null) {
+			$(".name").text(user.name);
+		}
+	});
+};

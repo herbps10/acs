efd9ff7aa9850c69743cecdba8c0ba95388b2269
Herb Susmann <herbps10@gmail.com>
2011-06-27 13:38:33 -0400

    Removed a lot of obsolute javascript code

--- public/script.js
+++ public/script.js
@@ -1,4 +1,17 @@
+// jQuery Code
+// Anything related to the Facebook buttons
+// should NOT be done here
+$(document).ready(function() {
+	$("a.update").click(function() {
+		$(this).remove();
+		$("form.update").show("slow");
 
+		return false;
+	});
+});
+
+
+// Facebook initialization
 (function() {
 var e = document.createElement('script');
 e.type = 'text/javascript';
@@ -8,7 +21,6 @@ e.async = true;
 document.getElementById('fb-root').appendChild(e);
 }());
 
-var logged_in = false;
 window.fbAsyncInit = function() {
 	// acs:4567
 	//FB.init({appId: '109163289177099', status: true, cookie: true,
@@ -18,48 +30,11 @@ window.fbAsyncInit = function() {
 	FB.init({appId: '195546887162731', status: true, cookie: true,
 	 	xfbml: true});
 
-
-	checkLoggedIn();
-
-	updateFromFacebook();
+	// Code that relies on Facebook can be called from here
 };
 
+// Fired when the user clicks on the facebook button
+// when already logged into facebook
 function onLogin() {
 	$("#already-registered").show("slow");
 }
-
-function checkLoggedIn() {
-	FB.getLoginStatus(function(response) {
-		if(response.session) {
-			//$(".login").remove();
-			logged_in = true;
-		}
-		else {
-			//$(".login").show();
-			logged_in = false;
-		}
-	});
-}
-
-function updateFromFacebook() {
-	FB.api('/me', function(user) {
-		if(user != null) {
-			$(".name").text(user.name);
-
-			updateForm(user);
-		}
-	});
-};
-
-function updateForm(user) {
-	$("form #facebook-id").val(user.id);
-}
-
-$(document).ready(function() {
-	$("a.update").click(function() {
-		$(this).remove();
-		$("form.update").show("slow");
-
-		return false;
-	});
-});
